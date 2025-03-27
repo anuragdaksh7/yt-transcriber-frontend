c36e28022b4cc1c9143cf815b3479dc925f110e8
Anurag Daksh <anuragdaksh77777@gmail.com>
2025-03-28 00:01:52 +0530

    random

--- components/TranscriptionFlow/TranscriptionOAuth/TranscriptionOAuth.tsx
+++ components/TranscriptionFlow/TranscriptionOAuth/TranscriptionOAuth.tsx
@@ -3,13 +3,21 @@ import ytSoundImage from "@/images/yt-sound-image.png"
 import styles from "./TranscriptionOAuth.module.scss"
 import OutlineButton from "@/ui/Buttons/OutlineButton/OutlineButton"
 import GoogleIcon from "@/images/google-icon.svg"
+import { useAppDispatch } from "@/lib/hooks"
+import { setCurrentPage } from "@/lib/features/currentDialog/currentDialogSlice"
+import { transcriberFlowState } from "@/contants/transcriberFlowState"
 
 const TranscriptionOAuth = () => {
+  const dispatch = useAppDispatch();
+
+  const onAuth = () => {
+    dispatch(setCurrentPage(transcriberFlowState.RESULT))
+  }
   return (
     <div className={styles.main_container}>
       <Image src={ytSoundImage} alt="Yt-Sound" priority={true} quality={100} width={138} />
       <h1>🔎 Instant Transcription – Sign in with Google to Begin</h1>
-      <OutlineButton>
+      <OutlineButton onClick={onAuth}>
         <Image src={GoogleIcon} quality={100} alt="google" width={24} />
         <p>Continue with google</p>
       </OutlineButton>
