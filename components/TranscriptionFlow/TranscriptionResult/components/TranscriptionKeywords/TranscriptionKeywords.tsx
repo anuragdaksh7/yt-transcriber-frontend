02148fde66da96475c589aab394b2fec8e871f37
Shivansh Agrawal <shivansh.agrprof@gmail.com>
2025-04-05 11:04:22 +0530

    Refactor styles and structure for Transcription components: enhance keyword display, improve styling consistency, and add new keyword section.

--- components/TranscriptionFlow/TranscriptionResult/components/TranscriptionKeywords/TranscriptionKeywords.tsx
+++ components/TranscriptionFlow/TranscriptionResult/components/TranscriptionKeywords/TranscriptionKeywords.tsx
@@ -1,19 +1,39 @@
-import Sparkle from "@/images/sparkle.svg"
-import Image from "next/image"
-import styles from "./TranscriptionKeywords.module.scss"
+import Sparkle from "@/images/sparkle.svg";
+import Image from "next/image";
+import styles from "./TranscriptionKeywords.module.scss";
 
 const TranscriptionKeywords = () => {
+  const hashtags = [
+    "#ScaryInteresting",
+    "#MountainStories",
+    "#DisturbingIncidents",
+    "#MountainClimbing",
+    "#Hiking",
+    "#SurvivalStories",
+    "#MissingPersons",
+    "#Hypothermia",
+    "#RescueOperation",
+    "#AlpineTragedy",
+  ];
+
   return (
     <div className={styles.main_container}>
       <div className={styles.header}>
-        <Image src={Sparkle} width={16} alt="AI"/>
+        <Image src={Sparkle} width={16} alt="AI" />
         <p className={styles.title}>SEO Keywords</p>
         <div className={styles.ai}>
           <p>AI</p>
         </div>
       </div>
+      <div className={styles.keywords}>
+        {hashtags.map((hashtag, index) => (
+          <span key={index} className={styles.hashtag}>
+            <p>{hashtag}</p>
+          </span>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TranscriptionKeywords
+export default TranscriptionKeywords;
